4090d5a68d1e7fc68d12e296565a376fa57cec1c
Compound-Z <nguyenductue2510@gmail.com>
2022-05-21 13:46:29 +0700

    08-add User model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const validator = require('validator')
+const UserSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'Please provide user name'],
+		minlength: 3,
+		maxlength: 50
+	},
+	email: {
+		type: String,
+		required: [true, 'Please provide email address'],
+		validate: {
+			validator: validator.isEmail,
+			message: 'Please provide valid email'
+		}
+	},
+	password: {
+		type: String,
+		required: [true, 'Please provide password'],
+		minlength: 8
+	},
+	role: {
+		type: String,
+		enum: ['admin', 'user'],
+		default: 'user'
+	}
+})
+
+module.exports = mongoose.model('User', UserSchema)
