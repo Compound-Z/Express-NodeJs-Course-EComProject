37a27d476cf4584471b3410ff007ce56221fa132
Compound-Z <nguyenductue2510@gmail.com>
2022-05-25 08:19:07 +0700

    get all reviews of a product

--- controllers/reviewCotroller.js
+++ controllers/reviewCotroller.js
@@ -11,7 +11,7 @@ const createReview = async (req, res) => {
 		throw new CustomError.NotFoundError('This product does not exist')
 	}
 
-	const isAlreadyHadReview = await Product.findOne({
+	const isAlreadyHadReview = await Review.findOne({
 		product: productId,
 		user: req.user.id
 	})
@@ -37,8 +37,22 @@ const getSingleReview = async (req, res) => {
 	res.status(StatusCodes.OK).json(review)
 }
 const updateReview = async (req, res) => {
-	console.log("updateReview")
-	res.send("updateReview")
+	const id = req.params.id
+	const review = await Review.findOne({ _id: id })
+	if (!review) {
+		throw new CustomError.NotFoundError(`Review with id ${id} doesn't exist`)
+	}
+
+	checkPermission(req.user, review.user)
+
+	const { rating, title, comment } = req.body
+	review.rating = rating
+	review.title = title
+	review.comment = comment
+
+	await review.save()
+
+	res.status(StatusCodes.OK).json({ review })
 }
 const deleteReview = async (req, res) => {
 	const id = req.params.id
@@ -48,8 +62,17 @@ const deleteReview = async (req, res) => {
 	}
 
 	checkPermission(req.user, review.user)
+
 	await review.remove()
+
 	res.status(StatusCodes.OK).json({ msg: "Review deleted!" })
 }
 
-module.exports = { createReview, getAllReviews, getSingleReview, updateReview, deleteReview }
+const getAllReviewsOfAProduct = async (req, res) => {
+	const { id } = req.params
+	console.log('id: ', id)
+	const reviews = await Review.find({ product: id })
+	console.log('reviews: ', reviews)
+	res.status(StatusCodes.OK).json(reviews)
+}
+module.exports = { createReview, getAllReviews, getSingleReview, updateReview, deleteReview, getAllReviewsOfAProduct }
--- routers/productRoutes.js
+++ routers/productRoutes.js
@@ -7,6 +7,7 @@ const {
 	deleteProduct
 } = require('../controllers/productController')
 
+const { getAllReviewsOfAProduct } = require('../controllers/reviewCotroller')
 const express = require('express')
 const router = express.Router()
 const authentication = require('../middleware/authentication')
@@ -21,5 +22,5 @@ router.route('/:id')
 	.get(getSingleProduct)
 	.patch(authentication.authenticateUser, authentication.authorizeUserPermission('admin'), updateProduct)
 	.delete(authentication.authenticateUser, authentication.authorizeUserPermission('admin'), deleteProduct)
-
+router.route('/:id/reviews').get(getAllReviewsOfAProduct)
 module.exports = router
